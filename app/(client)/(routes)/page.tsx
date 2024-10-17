72fedffa0e005964d6676af4b5b3bacbadb59cc7
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-10-17 18:17:10 +0800

    Add Product Listing Setup

--- app/(client)/(routes)/page.tsx
+++ app/(client)/(routes)/page.tsx
@@ -1,8 +1,26 @@
+import { getProducts } from "@/lib/data";
+import ProductListing from "./components/product-listing";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-export default function Home() {
-  return (
-    <section className="container mx-auto">
-      <h1 className="text-4xl">Home Page</h1>
-    </section>
-  );
+export default async function Home() {
+	const products = await getProducts();
+
+	return (
+		<section className="container mx-auto">
+			<h1 className="text-4xl py-6">Navbar</h1>
+
+			<Tabs defaultValue="shirts">
+				<TabsList>
+					<TabsTrigger value="shirts">Shirts</TabsTrigger>
+					<TabsTrigger value="tote-bags">Tote Bags</TabsTrigger>
+				</TabsList>
+				<TabsContent value="shirts">
+					<ProductListing products={products} />
+				</TabsContent>
+				<TabsContent value="tote-bags">
+					This will be tote-bags
+				</TabsContent>
+			</Tabs>
+		</section>
+	);
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'scontent.fmnl13-4.fna.fbcdn.net',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
