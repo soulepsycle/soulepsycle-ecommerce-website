580d8fa2b0ce5713ae4c81abc28b69c5e01a0acb
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-10-17 23:43:53 +0800

    Setup DB & prisma

---
+++ prisma/migrations/20241017154254_initial_migrate/migration.sql
@@ -0,0 +1,119 @@
+-- CreateEnum
+CREATE TYPE "PRODUCT_SIZE" AS ENUM ('S', 'M', 'L');
+
+-- CreateEnum
+CREATE TYPE "PRODUCT_STATUS" AS ENUM ('ACTIVE', 'ARCHIVE', 'DRAFT');
+
+-- CreateTable
+CREATE TABLE "Bag" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" UUID,
+
+    CONSTRAINT "Bag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BagItem" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "bag_id" UUID,
+    "product_variant_size" BIGINT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "BagItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" REAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "updated_at" TIMESTAMPTZ(6),
+    "category_id" UUID,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductVariantColor" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "product_id" UUID NOT NULL,
+    "color" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+    "images" TEXT[],
+
+    CONSTRAINT "ProductVariantColor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductVariantSize" (
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "stock" INTEGER NOT NULL,
+    "status" "PRODUCT_STATUS" NOT NULL,
+    "size" "PRODUCT_SIZE" NOT NULL,
+    "updated_at" TIMESTAMPTZ(6),
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "product_variant_color_id" UUID NOT NULL,
+
+    CONSTRAINT "ProductVariantSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductVariantColor_color_key" ON "ProductVariantColor"("color");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_password_key" ON "User"("password");
+
+-- AddForeignKey
+ALTER TABLE "Bag" ADD CONSTRAINT "Bag_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BagItem" ADD CONSTRAINT "BagItem_bag_id_fkey" FOREIGN KEY ("bag_id") REFERENCES "Bag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductVariantColor" ADD CONSTRAINT "ProductVariantColor_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductVariantSize" ADD CONSTRAINT "ProductVariantSize_product_variant_color_id_fkey" FOREIGN KEY ("product_variant_color_id") REFERENCES "ProductVariantColor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
