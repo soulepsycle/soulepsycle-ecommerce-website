94960ddf6ae38536c4c363c724917102b2be5a6c
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-10-17 23:44:54 +0800

    Add Create Product Page

---
+++ app/(private)/admin/(routes)/inventory/create/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductCreateForm from '../components/product-create-form'
+
+const ProductCreatePage = () => {
+  return (
+    <div>
+        <h1 className='text-4xl'>Create Product</h1>
+        <ProductCreateForm />
+    </div>
+  )
+}
+
+export default ProductCreatePage
