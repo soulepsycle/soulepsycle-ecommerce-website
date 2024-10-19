40aa9d855fce36e9d6172c1629bbb96ba5b5f6ad
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-10-19 08:07:32 +0800

    remove import of File and ListFilter

--- app/(private)/admin/(routes)/inventory/components/product-data-table.tsx
+++ app/(private)/admin/(routes)/inventory/components/product-data-table.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 
-import { File, ListFilter, PlusCircle } from "lucide-react";
+import { PlusCircle } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
