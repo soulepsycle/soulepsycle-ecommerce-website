a5ffc596bc01625b47b1a593021f424e54ef4281
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-10-23 22:15:07 +0800

    Refractor Data of Products & Add Component for Shop by Category

--- app/(client)/(routes)/(overview)/page.tsx
+++ app/(client)/(routes)/(overview)/page.tsx
@@ -1,19 +1,7 @@
+import { fetchProducts } from "@/lib/placeholder-data";
 import FeaturedByCategory from "../../components/featured-by-category";
 import FeaturedProducts from "../../components/featured-products";
 import Header from "../../components/header";
-
-const products = [
-	{ id: 1, name: "Classic Tote Bag", category: "tote-bag", price: 25.0 },
-	{ id: 2, name: "Canvas Market Tote", category: "tote-bag", price: 30.0 },
-	{ id: 3, name: "Eco-Friendly Tote", category: "tote-bag", price: 20.0 },
-	{ id: 4, name: "Stylish Beach Tote", category: "tote-bag", price: 35.0 },
-	{ id: 5, name: "Personalized Tote", category: "tote-bag", price: 28.0 },
-	{ id: 6, name: "Basic Cotton Shirt", category: "shirts", price: 15.0 },
-	{ id: 7, name: "Graphic Tee Shirt", category: "shirts", price: 20.0 },
-	{ id: 8, name: "Slim Fit Casual Shirt", category: "shirts", price: 22.0 },
-	{ id: 9, name: "Long Sleeve Shirt", category: "shirts", price: 25.0 },
-	{ id: 10, name: "Vintage Inspired Shirt", category: "shirts", price: 18.0 },
-];
 
 /*
 TODO:
@@ -22,6 +10,8 @@ TODO:
 */
 
 export default function Home() {
+	const products = fetchProducts();
+	
 	return (
 		<section>
 			<div className="container py-6">
--- app/(client)/(routes)/shop/[categoryName]/page.tsx
+++ app/(client)/(routes)/shop/[categoryName]/page.tsx
@@ -1,6 +1,10 @@
+import { fetchProducts } from "@/lib/placeholder-data";
+import Link from "next/link";
+import React from "react";
+import DummyImage from "@/public/images/600x400.png";
 import Header from "@/app/(client)/components/header";
 import { formatCategory } from "@/lib/helper/typography";
-import React from "react";
+import Image from "next/image";
 
 const ShopByCategoryName = ({
 	params,
@@ -10,11 +14,55 @@ const ShopByCategoryName = ({
 	};
 }) => {
 	const paramCategoryName = params.categoryName;
-	console.log(paramCategoryName);
+
+	const products = fetchProducts();
+
 	return (
 		<section>
 			<div className="container py-6">
-				<Header title={formatCategory(params.categoryName)}/>
+				<Header title={formatCategory(paramCategoryName)} />
+				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+					{products.filter(product => product.category === paramCategoryName).map((product) => {
+						const { id, name, category, price } = product;
+
+						return (
+							<Link
+								className="border rounded-md shadow-sm"
+								href={``}
+								key={id}
+							>
+								{/* header */}
+								<div className="relative w-full h-[400px]">
+									<Image
+										alt="Mountains"
+										src={DummyImage}
+										fill
+										sizes="(min-width: 808px) 50vw, 100vw"
+										style={{
+											objectFit: "cover", // cover, contain, none
+										}}
+									/>
+								</div>
+
+								{/* content */}
+								<div className="space-y-6 p-6">
+									{/* content-header */}
+									<div>
+										<h3 className="text-lg font-semibold">
+											{name}
+										</h3>
+										<h4 className="text-lg text-muted-foreground">
+											{category}
+										</h4>
+									</div>
+
+									{/* content-footer */}
+									<div>${price}</div>
+								</div>
+							</Link>
+						);
+					})}
+				</div>
 			</div>
 		</section>
 	);
